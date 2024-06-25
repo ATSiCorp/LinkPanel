4e299b8e8951b15d3384f20d8fbffc3ac75aed4e
Anjas amar pradana <anjastech01@gmail.com>
2024-06-26 03:45:20 +0700

    Fix Error Version Software

--- storage/app/linkpanel/setup.sh
+++ storage/app/linkpanel/setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-
-
 #################################################### CONFIGURATION ###
 BUILD=202112181
-PASS=???
-DBPASS=???
-SERVERID=???
+PASS=$(openssl rand -base64 32|sha256sum|base64|head -c 32| tr '[:upper:]' '[:lower:]')
+DBPASS=$(openssl rand -base64 24|sha256sum|base64|head -c 32| tr '[:upper:]' '[:lower:]')
+SERVERID=$(openssl rand -base64 12|sha256sum|base64|head -c 32| tr '[:upper:]' '[:lower:]')
 REPO=anjasamar/linkpanel
+if [ -z "$1" ];
+    BRANCH=main
+then
+    BRANCH=$1
+fi
 
 
 ####################################################   CLI TOOLS   ###
@@ -39,15 +42,15 @@ bgpurple=$(tput setab 5)
 clear
 echo "${green}${bold}"
 echo ""
-echo " ██████ ██ ██████  ██" 
-echo "██      ██ ██   ██ ██" 
-echo "██      ██ ██████  ██" 
-echo "██      ██ ██      ██" 
-echo " ██████ ██ ██      ██" 
-echo ""
+echo "█      █ ██████  █  █ ████     █     █████  █████ █    "
+echo "█      █ █     █ █ █  █   █   ███    █    █ █     █    "
+echo "█      █ █     █ ██   █   █  █   █   █    █ ███   █    "
+echo "█      █ █     █ █ █  ████  ███████  █    █ █     █    "
+echo "██████ █ █     █ █  █ █    █       █ █    █ █████ █████"
+echo "BY ATSi Corporation"
 echo "Installation has been started... Hold on!"
 echo "${reset}"
-sleep 3s
+sleep 10s
 
 
 
@@ -57,18 +60,18 @@ clear
 echo "${bggreen}${black}${bold}"
 echo "OS check..."
 echo "${reset}"
-sleep 1s
+sleep 10s
 
 ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
 VERSION=$(grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"')
 if [ "$ID" = "ubuntu" ]; then
     case $VERSION in
-        20.04)
+        24.04)
             break
             ;;
         *)
             echo "${bgred}${white}${bold}"
-            echo "LinkPanel requires Linux Ubuntu 20.04 LTS"
+            echo "LinkPanel requires Linux Ubuntu 24.04 LTS Only"
             echo "${reset}"
             exit 1;
             break
@@ -76,7 +79,7 @@ if [ "$ID" = "ubuntu" ]; then
     esac
 else
     echo "${bgred}${white}${bold}"
-    echo "LinkPanel requires Linux Ubuntu 20.04 LTS"
+    echo "LinkPanel requires Linux Ubuntu 24.04 LTS Only"
     echo "${reset}"
     exit 1
 fi
@@ -89,14 +92,14 @@ clear
 echo "${bggreen}${black}${bold}"
 echo "Permission check..."
 echo "${reset}"
-sleep 1s
+sleep 5s
 
 if [ "$(id -u)" = "0" ]; then
     clear
 else
     clear
     echo "${bgred}${white}${bold}"
-    echo "You have to run LinkPanel as root. (In AWS use 'sudo -s')"
+    echo "You have to run LinkPanel as root. (In VPS or Local Server use 'sudo -s')"
     echo "${reset}"
     exit 1
 fi
@@ -107,13 +110,24 @@ fi
 clear
 clear
 echo "${bggreen}${black}${bold}"
-echo "Base setup..."
+echo "OS Base setup also check Update - Upgrade - Install software for LinkPanel requirement..."
 echo "${reset}"
-sleep 1s
+sleep 15s
 
 sudo apt-get update
-sudo apt-get -y install software-properties-common curl wget nano vim rpl sed zip unzip openssl expect dirmngr apt-transport-https lsb-release ca-certificates dnsutils dos2unix zsh htop ffmpeg
+sudo apt-get upgrade
+sudo apt-get -y install software-properties-common curl wget nano micro vim rpl sed zip unzip openssl expect dirmngr apt-transport-https lsb-release ca-certificates dnsutils dos2unix zsh htop ffmpeg
 
+
+# GET IP
+clear
+clear
+echo "${bggreen}${black}${bold}"
+echo "Getting this machine public IP not local IP..."
+echo "${reset}"
+sleep 3s
+
+IP=$(curl -s https://checkip.amazonaws.com)
 
 
 # MOTD WELCOME MESSAGE
@@ -121,19 +135,21 @@ clear
 echo "${bggreen}${black}${bold}"
 echo "Motd settings..."
 echo "${reset}"
-sleep 1s
+sleep 5s
 
 WELCOME=/etc/motd
 sudo touch $WELCOME
 sudo cat > "$WELCOME" <<EOF
 
- ██████ ██ ██████  ██ 
-██      ██ ██   ██ ██ 
-██      ██ ██████  ██ 
-██      ██ ██      ██
- ██████ ██ ██      ██
+█      █ ██████  █  █ ████     █     █████  █████ █    
+█      █ █     █ █ █  █   █   ███    █    █ █     █    
+█      █ █     █ ██   █   █  █   █   █    █ ███   █    
+█      █ █     █ █ █  ████  ███████  █    █ █     █    
+██████ █ █     █ █  █ █    █       █ █    █ █████ █████
 
 With great power comes great responsibility...
+
+LinkPanel Build BY ATSi Corporation
 
 EOF
 
@@ -142,14 +158,15 @@ EOF
 # SWAP
 clear
 echo "${bggreen}${black}${bold}"
-echo "Memory SWAP..."
+echo "LinkPanel will set Memory SWAP to 2GB..."
+sleep 5s
+
+sudo /bin/dd if=/dev/zero of=/var/swap.LinkPanel2GB bs=2M count=2024
+sudo /sbin/mkswap /var/swap.LinkPanel2GB
+sudo /sbin/swapon /var/swap.LinkPanel2GB
+sudo free -h
+sleep 5s
 echo "${reset}"
-sleep 1s
-
-sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
-sudo /sbin/mkswap /var/swap.1
-sudo /sbin/swapon /var/swap.1
-
 
 
 # ALIAS
@@ -157,7 +174,7 @@ clear
 echo "${bggreen}${black}${bold}"
 echo "Custom CLI configuration..."
 echo "${reset}"
-sleep 1s
+sleep 10s
 
 shopt -s expand_aliases
 alias ll='ls -alF'
@@ -167,9 +184,9 @@ alias ll='ls -alF'
 # LINKPANEL DIRS
 clear
 echo "${bggreen}${black}${bold}"
-echo "LinkPanel directories..."
+echo "Configure LinkPanel directories..."
 echo "${reset}"
-sleep 1s
+sleep 10s
 
 sudo mkdir /etc/linkpanel/
 sudo chmod o-r /etc/linkpanel
@@ -181,9 +198,9 @@ sudo chmod o-r /var/linkpanel
 # USER
 clear
 echo "${bggreen}${black}${bold}"
-echo "LinkPanel root user..."
+echo "Set LinkPanel root user..."
 echo "${reset}"
-sleep 1s
+sleep 10s
 
 sudo pam-auth-update --package
 sudo mount -o remount,rw /
@@ -193,20 +210,20 @@ echo "linkpanel:$PASS"|sudo chpasswd
 sudo usermod -aG sudo linkpanel
 
 
-
-
 # NGINX
 clear
 echo "${bggreen}${black}${bold}"
-echo "nginx setup..."
+echo "Nginx setup..."
 echo "${reset}"
-sleep 1s
+sleep 5s
 
-sudo apt-get -y install nginx-core
+sudo apt-get -y install nginx
 sudo systemctl start nginx.service
 sudo rpl -i -w "http {" "http { limit_req_zone \$binary_remote_addr zone=one:10m rate=1r/s; fastcgi_read_timeout 300;" /etc/nginx/nginx.conf
 sudo rpl -i -w "http {" "http { limit_req_zone \$binary_remote_addr zone=one:10m rate=1r/s; fastcgi_read_timeout 300;" /etc/nginx/nginx.conf
 sudo systemctl enable nginx.service
+sudo systemctl status nginx.service
+sleep 5s
 
 
 
@@ -215,9 +232,9 @@ sudo systemctl enable nginx.service
 # FIREWALL
 clear
 echo "${bggreen}${black}${bold}"
-echo "fail2ban setup..."
+echo "Fail2ban Firewall setup..."
 echo "${reset}"
-sleep 1s
+sleep 5s
 
 sudo apt-get -y install fail2ban
 JAIL=/etc/fail2ban/jail.local
@@ -244,14 +261,14 @@ sudo ufw allow "Nginx Full"
 # PHP
 clear
 echo "${bggreen}${black}${bold}"
-echo "PHP setup..."
+echo "PHP setup (This may take some time for install and configure)"
 echo "${reset}"
-sleep 1s
+sleep 15s
 
 
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update
-
+sleep 5s
 sudo apt-get -y install php7.4-fpm
 sudo apt-get -y install php7.4-common
 sudo apt-get -y install php7.4-curl
@@ -279,8 +296,8 @@ sudo cat > "$PHPINI" <<EOF
 memory_limit = 256M
 upload_max_filesize = 256M
 post_max_size = 256M
-max_execution_time = 180
-max_input_time = 180
+max_execution_time = 1999
+max_input_time = 1999
 EOF
 sudo service php7.4-fpm restart
 
@@ -311,11 +328,10 @@ sudo cat > "$PHPINI" <<EOF
 memory_limit = 256M
 upload_max_filesize = 256M
 post_max_size = 256M
-max_execution_time = 180
-max_input_time = 180
+max_execution_time = 1999
+max_input_time = 1999
 EOF
 sudo service php8.0-fpm restart
-
 
 sudo apt-get -y install php8.1-fpm
 sudo apt-get -y install php8.1-common
@@ -344,14 +360,42 @@ sudo cat > "$PHPINI" <<EOF
 memory_limit = 256M
 upload_max_filesize = 256M
 post_max_size = 256M
-max_execution_time = 180
-max_input_time = 180
+max_execution_time = 1999
+max_input_time = 1999
 EOF
 sudo service php8.1-fpm restart
 
-
-# PHP EXTRA
-sudo apt-get -y install php-dev php-pear
+sudo apt-get -y install php8.2-fpm
+sudo apt-get -y install php8.2-common
+sudo apt-get -y install php8.2-curl
+sudo apt-get -y install php8.2-openssl
+sudo apt-get -y install php8.2-bcmath
+sudo apt-get -y install php8.2-mbstring
+sudo apt-get -y install php8.2-tokenizer
+sudo apt-get -y install php8.2-mysql
+sudo apt-get -y install php8.2-sqlite3
+sudo apt-get -y install php8.2-pgsql
+sudo apt-get -y install php8.2-redis
+sudo apt-get -y install php8.2-memcached
+sudo apt-get -y install php8.2-json
+sudo apt-get -y install php8.2-zip
+sudo apt-get -y install php8.2-xml
+sudo apt-get -y install php8.2-soap
+sudo apt-get -y install php8.2-gd
+sudo apt-get -y install php8.2-imagick
+sudo apt-get -y install php8.2-fileinfo
+sudo apt-get -y install php8.2-imap
+sudo apt-get -y install php8.2-cli
+PHPINI=/etc/php/8.2/fpm/conf.d/linkpanel.ini
+sudo touch $PHPINI
+sudo cat > "$PHPINI" <<EOF
+memory_limit = 256M
+upload_max_filesize = 256M
+post_max_size = 256M
+max_execution_time = 1999
+max_input_time = 1999
+EOF
+sudo service php8.2-fpm restart
 
 
 # PHP EXTRA
@@ -363,9 +407,9 @@ clear
 echo "${bggreen}${black}${bold}"
 echo "PHP CLI configuration..."
 echo "${reset}"
-sleep 1s
+sleep 10s
 
-sudo update-alternatives --set php /usr/bin/php8.0
+sudo update-alternatives --set php /usr/bin/php8.2
 
 
 
@@ -374,7 +418,7 @@ clear
 echo "${bggreen}${black}${bold}"
 echo "Composer setup..."
 echo "${reset}"
-sleep 1s
+sleep 10s
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php --no-interaction
@@ -390,7 +434,7 @@ clear
 echo "${bggreen}${black}${bold}"
 echo "GIT setup..."
 echo "${reset}"
-sleep 1s
+sleep 10s
 
 sudo apt-get -y install git
 sudo ssh-keygen -t rsa -C "git@github.com" -f /etc/linkpanel/github -q -P ""
@@ -402,7 +446,7 @@ clear
 echo "${bggreen}${black}${bold}"
 echo "Supervisor setup..."
 echo "${reset}"
-sleep 1s
+sleep 10s
 
 sudo apt-get -y install supervisor
 service supervisor restart
@@ -414,7 +458,7 @@ clear
 echo "${bggreen}${black}${bold}"
 echo "Default vhost..."
 echo "${reset}"
-sleep 1s
+sleep 10s
 
 NGINX=/etc/nginx/sites-available/default
 if test -f "$NGINX"; then
@@ -425,7 +469,7 @@ sudo cat > "$NGINX" <<EOF
 server {
     listen 80 default_server;
     listen [::]:80 default_server;
-    root /var/www/html;
+    root /var/www/html/public;
     add_header X-Frame-Options "SAMEORIGIN";
     add_header X-XSS-Protection "1; mode=block";
     add_header X-Content-Type-Options "nosniff";
@@ -452,6 +496,8 @@ server {
 EOF
 sudo mkdir /etc/nginx/linkpanel/
 sudo systemctl restart nginx.service
+sudo systemctl status nginx.service
+sleep 5s
 
 
 
@@ -462,7 +508,7 @@ clear
 echo "${bggreen}${black}${bold}"
 echo "MySQL setup..."
 echo "${reset}"
-sleep 1s
+sleep 5s
 
 
 sudo apt-get install -y mysql-server
@@ -500,11 +546,13 @@ clear
 echo "${bggreen}${black}${bold}"
 echo "Redis setup..."
 echo "${reset}"
-sleep 1s
+sleep 5s
 
 sudo apt install -y redis-server
 sudo rpl -i -w "supervised no" "supervised systemd" /etc/redis/redis.conf
 sudo systemctl restart redis.service
+sudo systemctl status redis.service
+sleep 5s
 
 
 
@@ -513,7 +561,7 @@ clear
 echo "${bggreen}${black}${bold}"
 echo "Let's Encrypt setup..."
 echo "${reset}"
-sleep 1s
+sleep 5s
 
 sudo apt-get install -y certbot
 sudo apt-get install -y python3-certbot-nginx
@@ -525,16 +573,16 @@ clear
 echo "${bggreen}${black}${bold}"
 echo "Node/npm setup..."
 echo "${reset}"
-sleep 1s
+sleep 5s
 
 curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 NODE=/etc/apt/sources.list.d/nodesource.list
 sudo unlink NODE
 sudo touch $NODE
 sudo cat > "$NODE" <<EOF
-deb https://deb.nodesource.com/node_16.x focal main
-deb-src https://deb.nodesource.com/node_16.x focal main
+deb https://deb.nodesource.com/node_20.x noble main
+deb-src https://deb.nodesource.com/node_20.x noble main
 EOF
 sudo apt-get update
 sudo apt -y install nodejs
@@ -543,15 +591,82 @@ sudo apt -y install npm
 
 
 
+#PANEL INSTALLATION
+clear
+echo "${bggreen}${black}${bold}"
+echo "And now LinkPanel installation begin..."
+echo "${reset}"
+sleep 5s
+
+
+/usr/bin/mysql -u root -p$DBPASS <<EOF
+CREATE DATABASE IF NOT EXISTS linkpanel;
+EOF
+clear
+sudo rm -rf /var/www/html
+cd /var/www && git clone https://github.com/$REPO.git html
+cd /var/www/html && git pull
+cd /var/www/html && git checkout $BRANCH
+cd /var/www/html && git pull
+cd /var/www/html && sudo unlink .env
+cd /var/www/html && sudo cp .env.example .env
+cd /var/www/html && php artisan key:generate
+sudo rpl -i -w "DB_USERNAME=dbuser" "DB_USERNAME=linkpanel" /var/www/html/.env
+sudo rpl -i -w "DB_PASSWORD=dbpass" "DB_PASSWORD=$DBPASS" /var/www/html/.env
+sudo rpl -i -w "DB_DATABASE=dbname" "DB_DATABASE=linkpanel" /var/www/html/.env
+sudo rpl -i -w "APP_URL=http://localhost" "APP_URL=http://$IP" /var/www/html/.env
+sudo rpl -i -w "APP_ENV=local" "APP_ENV=production" /var/www/html/.env
+sudo rpl -i -w "LINKPANELSERVERID" $SERVERID /var/www/html/database/seeders/DatabaseSeeder.php
+sudo rpl -i -w "LINKPANELIP" $IP /var/www/html/database/seeders/DatabaseSeeder.php
+sudo rpl -i -w "LINKPANELPASS" $PASS /var/www/html/database/seeders/DatabaseSeeder.php
+sudo rpl -i -w "LINKPANELDB" $DBPASS /var/www/html/database/seeders/DatabaseSeeder.php
+sudo chmod -R o+w /var/www/html/storage
+sudo chmod -R 777 /var/www/html/storage
+sudo chmod -R o+w /var/www/html/bootstrap/cache
+sudo chmod -R 777 /var/www/html/bootstrap/cache
+cd /var/www/html && composer update --no-interaction
+cd /var/www/html && php artisan key:generate
+cd /var/www/html && php artisan cache:clear
+cd /var/www/html && php artisan storage:link
+cd /var/www/html && php artisan view:cache
+cd /var/www/html && php artisan linkpanel:activesetupcount
+LINKPANELBULD=/var/www/html/public/build_$SERVERID.php
+sudo touch $LINKPANELBULD
+sudo cat > $LINKPANELBULD <<EOF
+$BUILD
+EOF
+LINKPANELPING=/var/www/html/public/ping_$SERVERID.php
+sudo touch $LINKPANELPING
+sudo cat > $LINKPANELPING <<EOF
+Up
+EOF
+PUBKEYGH=/var/www/html/public/ghkey_$SERVERID.php
+sudo touch $PUBKEYGH
+sudo cat > $PUBKEYGH <<EOF
+<?php
+echo exec("cat /etc/linkpanel/github.pub");
+EOF
+cd /var/www/html && php artisan migrate --seed --force
+cd /var/www/html && php artisan config:cache
+sudo chmod -R o+w /var/www/html/storage
+sudo chmod -R 775 /var/www/html/storage
+sudo chmod -R o+w /var/www/html/bootstrap/cache
+sudo chmod -R 775 /var/www/html/bootstrap/cache
+sudo chown -R www-data:linkpanel /var/www/html
+
+
+
 # LAST STEPS
 clear
 echo "${bggreen}${black}${bold}"
-echo "Last steps..."
+echo "Last LinkPanel installation steps..."
 echo "${reset}"
 sleep 1s
 
 sudo chown www-data:linkpanel -R /var/www/html
 sudo chmod -R 750 /var/www/html
+sudo echo 'DefaultStartLimitIntervalSec=1s' >> /usr/lib/systemd/system/user@.service
+sudo echo 'DefaultStartLimitBurst=50' >> /usr/lib/systemd/system/user@.service
 sudo echo 'StartLimitBurst=0' >> /usr/lib/systemd/system/user@.service
 sudo systemctl daemon-reload
 
@@ -573,31 +688,26 @@ sudo rpl -i -w "# PasswordAuthentication" "PasswordAuthentication" /etc/ssh/sshd
 sudo rpl -i -w "PasswordAuthentication no" "PasswordAuthentication yes" /etc/ssh/sshd_config
 sudo rpl -i -w "PermitRootLogin yes" "PermitRootLogin no" /etc/ssh/sshd_config
 sudo service sshd restart
-wget -P /var/www/html/ - https://raw.githubusercontent.com/$REPO/latest/utility/zero-page/index.php
-LINKPANELBULD=/var/www/html/build_$SERVERID.php
-sudo touch $LINKPANELBULD
-sudo cat > "$LINKPANELBULD" <<EOF
-$BUILD
+TASK=/etc/supervisor/conf.d/linkpanel.conf
+touch $TASK
+cat > "$TASK" <<EOF
+[program:linkpanel-worker]
+process_name=%(program_name)s_%(process_num)02d
+command=php /var/www/html/artisan queue:work --sleep=3 --tries=3 --max-time=3600
+autostart=true
+autorestart=true
+stopasgroup=true
+killasgroup=true
+user=linkpanel
+numprocs=8
+redirect_stderr=true
+stdout_logfile=/var/www/worker.log
+stopwaitsecs=3600
 EOF
-LINKPANELPING=/var/www/html/ping_$SERVERID.php
-sudo touch $LINKPANELPING
-sudo cat > "$LINKPANELPING" <<EOF
-Up
-EOF
-ARTISAN=/var/www/html/artisan
-sudo touch $ARTISAN
-UPDATE=/var/www/html/utility/linkpanel-update/run.sh
-sudo touch $UPDATE
-PUBKEYGH=/var/www/html/ghkey_$SERVERID.php
-sudo touch $PUBKEYGH
-sudo cat > "$PUBKEYGH" <<EOF
-<?php
-echo exec("cat /etc/linkpanel/github.pub");
-EOF
-
-
-
-
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl start all
+sudo service supervisor restart
 
 # COMPLETE
 clear
@@ -615,10 +725,14 @@ echo "***********************************************************"
 echo "                    SETUP COMPLETE"
 echo "***********************************************************"
 echo ""
-echo " SSH root user: linkpanel"
+echo " SSH root user: atsi"
 echo " SSH root pass: $PASS"
-echo " MySQL root user: linkpanel"
+echo " MySQL root user: atsi"
 echo " MySQL root pass: $DBPASS"
+echo ""
+echo " To manage your server visit: http://$IP"
+echo " and click on 'dashboard' button."
+echo " Default credentials are: admin / admin1230"
 echo ""
 echo "***********************************************************"
 echo "          DO NOT LOSE AND KEEP SAFE THIS DATA"
