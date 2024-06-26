fbc2de82c57162fd39b7103c170a78881fbd99d3
Anjas amar pradana <anjastech01@gmail.com>
2024-06-26 10:53:41 +0700

    up

--- installer-ubuntu.sh
+++ installer-ubuntu.sh
@@ -5,7 +5,7 @@ BUILD=202112181
 PASS=$(openssl rand -base64 32|sha256sum|base64|head -c 32| tr '[:upper:]' '[:lower:]')
 DBPASS=$(openssl rand -base64 24|sha256sum|base64|head -c 32| tr '[:upper:]' '[:lower:]')
 SERVERID=$(openssl rand -base64 12|sha256sum|base64|head -c 32| tr '[:upper:]' '[:lower:]')
-REPO=ATSiCorp/linkpanel
+REPO=ATSiCorp/LinkPanel
 if [ -z "$1" ];
     BRANCH=main
 then
